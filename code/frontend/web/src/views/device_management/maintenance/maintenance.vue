d08b010efaf777e2ede3d0103d62bd963e3a7140
maxiaoyan <2696318766@qq.com>
2021-08-13 21:11:56 +0800

    更新 'code/frontend/web/src/views/device_management/maintenance/maintenance.vue'

--- code/frontend/web/src/views/device_management/maintenance/maintenance.vue
+++ code/frontend/web/src/views/device_management/maintenance/maintenance.vue
@@ -227,20 +227,6 @@ export default {
       })
       this.list.splice(index, 1)
     },
-    handleDownload() {
-      this.downloadLoading = true
-      import('@/vendor/Export2Excel').then(excel => {
-        const tHeader = ['timestamp', 'title', 'type', 'importance', 'status']
-        const filterVal = ['timestamp', 'title', 'type', 'importance', 'status']
-        const data = this.formatJson(filterVal)
-        excel.export_json_to_excel({
-          header: tHeader,
-          data,
-          filename: 'table-list'
-        })
-        this.downloadLoading = false
-      })
-    },
     formatJson(filterVal) {
       return this.list.map(v => filterVal.map(j => {
           return v[j]
