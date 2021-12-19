6a18c327a76d28f132f9cf5bcb8823e82bce2622
Y.Nie <nie.yao.work@outlook.com>
2021-12-19 23:11:46 +0800

    复制代码

---
+++ code/frontend/web/src/views/dashboard/NumberPanel.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-card shadow="hover" class="number-card-box" :style="getBackgroundColor">
+    <div slot="header">{{ title }}</div>
+    <div align="center" class="number-text">{{ number }}</div>
+  </el-card>
+</template>
+
+<script>
+export default {
+  props: ["title", "number", "background-color"],
+  computed: {
+    getBackgroundColor: function () {
+      return "background: " + this.backgroundColor;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.number-card-box {
+  border-radius: 10%;
+}
+
+.number-text {
+  font-size: 30px;
+}
+</style>
