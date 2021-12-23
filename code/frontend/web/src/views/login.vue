52b38e2b8aca56b7d6eedf153aff6d3478a18b87
Ameame <g2niko@163.com>
2021-12-23 14:17:01 +0800

    测试解决合并冲突3

--- code/frontend/web/src/views/login.vue
+++ code/frontend/web/src/views/login.vue
@@ -1,16 +1,69 @@
 <template>
   <div class="login">
     <!-- 登录 -->
-    <login-form
-      :display="display.login"
-      @register="handleRegister"
-    ></login-form>
-    <!-- 注册 -->
-    <register-form
-      :display="display.register"
-      @return="handleReturn"
-      @display="handleDisplayChange"
-    ></register-form>
+    <el-form
+      ref="loginForm"
+      :model="loginForm"
+      :rules="loginRules"
+      class="login-form"
+    >
+      <h3 class="title">南京市城市轨道交通线网实训基地<br />培训管理系统</h3>
+      <!-- 账号 -->
+      <el-form-item prop="username">
+        <el-input
+          v-model="loginForm.username"
+          type="text"
+          auto-complete="off"
+          placeholder="账号"
+        >
+          <svg-icon
+            slot="prefix"
+            icon-class="user"
+            class="el-input__icon input-icon"
+          />
+        </el-input>
+      </el-form-item>
+      <!-- 密码 -->
+      <el-form-item prop="password">
+        <el-input
+          v-model="loginForm.password"
+          type="password"
+          auto-complete="off"
+          placeholder="密码"
+          @keyup.enter.native="handleLogin"
+        >
+          <svg-icon
+            slot="prefix"
+            icon-class="password"
+            class="el-input__icon input-icon"
+          />
+        </el-input>
+      </el-form-item>
+      <!-- 记住密码 -->
+      <el-checkbox v-model="loginForm.rememberMe" style="margin-bottom: 25px"
+        >记住密码</el-checkbox
+      >
+      <el-form-item style="width: 100%">
+        <!-- 登录 -->
+        <el-col :span="12" align="center">
+          <el-button
+            :loading="loading"
+            size="medium"
+            type="primary"
+            @click.native.prevent="handleLogin"
+          >
+            <span v-if="!loading">登 录</span>
+            <span v-else>登 录 中...</span>
+          </el-button>
+        </el-col>
+        <!-- 注册 -->
+        <el-col :span="12" align="center">
+          <el-button size="medium" @click.native.prevent="handleRegister">
+            <span>注 册</span>
+          </el-button>
+        </el-col>
+      </el-form-item>
+    </el-form>
 
     <!--  底部  -->
     <div class="el-login-footer">
@@ -23,19 +76,13 @@
 import { getCodeImg } from "@/api/login";
 import Cookies from "js-cookie";
 import { encrypt, decrypt } from "@/utils/jsencrypt";
-import LoginForm from "./login/LoginForm.vue";
-import RegisterForm from "./login/RegisterForm.vue";
 
 export default {
-  components: { LoginForm, RegisterForm },
   name: "Login",
   data() {
     return {
-      display: { login: true, register: false },
-      codeUrl: "",
-      cookiePassword: "",
       loginForm: {
-        username: "adminavp",
+        username: "admin",
         password: "admin123",
         rememberMe: false,
         code: "",
@@ -54,6 +101,8 @@ export default {
       },
       loading: false,
       redirect: undefined,
+      codeUrl: "",
+      cookiePassword: "",
     };
   },
   watch: {
@@ -64,18 +113,53 @@ export default {
       immediate: true,
     },
   },
-  created() {},
+  created() {
+    //this.getCode();
+    this.getCookie();
+  },
   methods: {
+    getCookie() {
+      const username = Cookies.get("username");
+      const password = Cookies.get("password");
+      const rememberMe = Cookies.get("rememberMe");
+      this.loginForm = {
+        username: username === undefined ? this.loginForm.username : username,
+        password:
+          password === undefined ? this.loginForm.password : decrypt(password),
+        rememberMe: rememberMe === undefined ? false : Boolean(rememberMe),
+      };
+    },
+    handleLogin() {
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          this.loading = true;
+          if (this.loginForm.rememberMe) {
+            Cookies.set("username", this.loginForm.username, { expires: 30 });
+            Cookies.set("password", encrypt(this.loginForm.password), {
+              expires: 30,
+            });
+            Cookies.set("rememberMe", this.loginForm.rememberMe, {
+              expires: 30,
+            });
+          } else {
+            Cookies.remove("username");
+            Cookies.remove("password");
+            Cookies.remove("rememberMe");
+          }
+          this.$store
+            .dispatch("Login", this.loginForm)
+            .then(() => {
+              this.$router.push({ path: this.redirect || "/" }).catch(() => {});
+            })
+            .catch(() => {
+              this.loading = false;
+              //this.getCode();
+            });
+        }
+      });
+    },
     handleRegister() {
-      this.display.login = false;
-      this.display.register = true;
-    },
-    handleReturn() {
-      this.display.register = false;
-      this.display.login = true;
-    },
-    handleDisplayChange(value) {
-      this.display.register = value;
+      this.$router.push({ path: "/register" }).catch(() => {});
     },
   },
 };
@@ -95,7 +179,6 @@ export default {
   text-align: center;
   color: #707070;
 }
-
 .login-form {
   border-radius: 6px;
   background: #fbfdfd;
