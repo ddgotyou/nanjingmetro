8c4dd1d650a91ae9dd112644903587f9a25940c8
lingbai-kong <1577347854@qq.com>
2022-01-18 12:05:10 +0800

    更新计划实施调整页面弹窗

--- code/frontend/web/src/views/plan_organization/plan_implement/plan_implement.vue
+++ code/frontend/web/src/views/plan_organization/plan_implement/plan_implement.vue
@@ -137,27 +137,48 @@
             </el-col>
           </el-row>
           <el-row>
-            <el-col :span="12">
+            <el-col :span="24">
               <el-form-item label="课时安排">
                 <el-date-picker
-                  style="width:100%;"
-                  value-format="yyyy-MM-dd HH:mm:ss"
-                  v-model="taskData.time"
-                  type="datetimerange"
-                  range-separator="至"
-                  start-placeholder="开始日期"
-                  end-placeholder="结束日期">
+                  style="width:50%;"
+                  value-format="yyyy-MM-dd"
+                  v-model="taskData.date"
+                  type="date"
+                  placeholder="选择日期"
+                  :picker-options="taskPeriodOptions"
+                  @change="changeDate">
                 </el-date-picker>
+                <el-time-picker
+                  style="width:25%;"
+                  placeholder="起始时间"
+                  format="HH:mm"
+                  value-format="HH:mm:00"
+                  :disabled="taskData.date==null"
+                  v-model="taskData.period[0]"
+                  :picker-options="{
+                    selectableRange: startTimeRange
+                  }"
+                  @change="changeStartTime">
+                </el-time-picker>
+                <el-time-picker
+                  style="width:25%;"
+                  placeholder="结束时间"
+                  format="HH:mm"
+                  value-format="HH:mm:00"
+                  :disabled="taskData.date==null||taskData.period[0]==null"
+                  v-model="taskData.period[1]"
+                  :picker-options="{
+                    selectableRange: endTimeRange
+                  }"
+                  @change="changeEndTime">
+                </el-time-picker>
               </el-form-item>
-              <!-- <el-form-item label="课时安排">
-                <el-input id="task_time" v-model="taskData.time" name="task_time" />
-              </el-form-item> -->
             </el-col>
-            <el-col :span="12">
+            <!-- <el-col :span="12">
               <el-form-item label="顺序">
                 <el-input-number style="width:100%;" v-model="taskData.order" :min="1" :max="100"></el-input-number>
               </el-form-item>
-            </el-col>
+            </el-col> -->
           </el-row>
           <el-row>
             <el-col :span="12">
@@ -188,7 +209,7 @@
           <el-row>
             <el-col :span="12">
               <el-form-item label="教室">
-                <el-select v-model="taskData.classroom" style="width:100%" placeholder="请选择">
+                <el-select v-model="taskData.classroom" style="width:100%" placeholder="请选择" clearable @change="changeClassroom()">
                   <el-option
                     v-for="item in classrooms"
                     :key="item.value"
@@ -212,9 +233,13 @@
           style="width: 100"
         >
           <el-table-column
+            type="index"
+            width="50">
+          </el-table-column>
+          <!-- <el-table-column
             prop="order"
             label="任务顺序"
-          />
+          /> -->
           <el-table-column
             prop="name"
             label="任务名称"
@@ -235,7 +260,7 @@
               <el-button
                 type="text"
                 size="small"
-                @click.native.prevent="deleteRow(scope.$index, taskTableData)"
+                @click.native.prevent="deleteRow(scope.$index, tableData)"
               >
                 移除
               </el-button>
@@ -298,8 +323,9 @@ export default {
       taskData: {
         name: '',
         option: '',
-        order: '',
-        time: ['',''],
+        // order: '',
+        date: null,
+        period: [null,null],
         type: '',
         score: '',
         classroom: '',
@@ -309,11 +335,28 @@ export default {
       taskTableData: [],
       
       dialogTableVisible: false,
-      dialogFormVisible: false
+      dialogFormVisible: false,
+
+      first_choose: '',
+      periods: [],
+      startTimeRange: [],
+      endTimeRange: []
     }
   },
   created(){
+    this.getSelection()
     this.list()
+  },
+  computed: {
+    taskPeriodOptions() {
+      return {
+        disabledDate(time) {
+          var timeNow = Date.now()
+          var before=timeNow-24*60*60*1000
+          return !(time.getTime()>=before)
+        }
+      }
+    }
   },
   methods: {
     fresh(){
@@ -422,8 +465,9 @@ export default {
       this.taskData={
         name: '',
         option: '',
-        order: '',
-        time: ['',''],
+        // order: '',
+        date: null,
+        period: [null,null],
         type: '',
         score: '',
         classroom: '',
@@ -448,23 +492,28 @@ export default {
       this.dialogFormVisible = true;
     },
     addTask() {
-      this.taskTableData.push({
-        classroom: this.taskData.classroom,
-        name: this.taskData.name,
-        chooseTask: this.taskData.option,
-        type: this.taskData.type,
-        taskScore: this.taskData.score,
-        inPlanTask: null,
-        description: this.taskData.description,
-        startTime: this.taskData.time[0],
-        endTime: this.taskData.time[1],
-        order: this.taskData.order,
-        signInNumber: null,
-        signOutNumber: null
-      })
-      this.taskTableData.sort(function (a,b) {
-        return a.order-b.order;
-      })
+      if(this.taskData.name==''||this.taskData.option==''||this.taskData.date==null||this.taskData.period[0]==null||this.taskData.period[1]==null||this.taskData.type==''||this.taskData.score==' '||this.taskData.classroom==''||this.taskData.description==''){
+        this.$message.error('表单内存在空值！');
+      }
+      else{
+        this.taskTableData.push({
+          classroom: this.taskData.classroom,
+          name: this.taskData.name,
+          chooseTask: this.taskData.option,
+          type: this.taskData.type,
+          taskScore: this.taskData.score,
+          inPlanTask: null,
+          description: this.taskData.description,
+          startTime: this.taskData.date+' '+this.taskData.period[0],
+          endTime: this.taskData.date+' '+this.taskData.period[1],
+          // order: this.taskData.order,
+          signInNumber: null,
+          signOutNumber: null
+        })
+        // this.tableData.sort(function (a,b) {
+        //   return a.order-b.order;
+        // })
+      }
     },
     deleteRow(index, taskTableData) {
       this.taskTableData.splice(index, 1)
@@ -513,6 +562,145 @@ export default {
         });
         console.log("update plan successfully!")
       })
+    },
+    changeDate() {
+      if(this.first_choose=='' || this.first_choose=='period') {
+        //清空或未选择
+        if(this.taskData.date==null) {
+          this.taskData.period=[null,null]
+          this.first_choose=''
+          this.taskData.classroom=''
+          this.startTimeRange=[]
+          this.endTimeRange=[]
+          var that=this
+          api.classrooms().then( res => {
+            that.classrooms=[]
+            for(var i=0;i<res._embedded.classrooms.length;i++)
+            {
+              that.classrooms.push({label:res._embedded.classrooms[i].name,value:res._embedded.classrooms[i].id})
+            }
+          })
+        }
+        else if(this.taskData.period[0]==null)
+        {
+          this.taskData.period[1]=null
+        }
+        else if(this.taskData.date!=null&&this.taskData.period[0]!=null&&this.taskData.period[1]!=null) {
+        //选择了日期
+          this.first_choose='period'
+          var that=this
+          api.findClassrooms({
+            date: that.taskData.date,
+            startTime: that.taskData.period[0],
+            endTime: that.taskData.period[1]
+          }).then( res => {
+            that.classrooms=[]
+            for(var i=0;i<res._embedded.classrooms.length;i++)
+            {
+              that.classrooms.push({label:res._embedded.classrooms[i].name,value:res._embedded.classrooms[i].id})
+            }
+          })
+        }
+      }
+      else if(this.first_choose=='classroom')
+      {
+        var that=this
+        that.taskData.period=[null,null]
+        if (that.taskData.date!=null) {
+          api.findAvailableTime({
+            id: that.taskData.classroom,
+            date: that.taskData.date
+          }).then( res => {
+            that.periods=[]
+            that.startTimeRange=[]
+            for(var i=0;i<res._embedded.periods.length;i++)
+            {
+              that.periods.push({startTime:res._embedded.periods[i].startTime.substr(11,19),endTime:res._embedded.periods[i].endTime.substr(11,19)})
+              that.startTimeRange.push(res._embedded.periods[i].startTime.substr(11,19)+' - '+res._embedded.periods[i].endTime.substr(11,19))
+            }
+          })
+        }
+      }
+    },
+    changeStartTime() {
+      if(this.first_choose=='' || this.first_choose=='period') {
+        //清空或未选择
+        if(this.taskData.period[0]==null)
+        {
+          this.taskData.period[1]=null
+        }
+        else if(this.taskData.date!=null&&this.taskData.period[0]!=null&&this.taskData.period[1]!=null) {
+        //选择了日期
+          this.first_choose='period'
+          var that=this
+          api.findClassrooms({
+            date: that.taskData.date,
+            startTime: that.taskData.period[0],
+            endTime: that.taskData.period[1]
+          }).then( res => {
+            that.classrooms=[]
+            for(var i=0;i<res._embedded.classrooms.length;i++)
+            {
+              that.classrooms.push({label:res._embedded.classrooms[i].name,value:res._embedded.classrooms[i].id})
+            }
+          })
+        }
+      }
+      else if(this.first_choose=='classroom')
+      {
+        var that=this
+        that.taskData.period[1]=null
+        if (that.taskData.period[0]!=null) {
+          for(var i=0;i<that.periods.length;i++)
+          {
+            // if(new Date(that.taskData.period[0]).getTime() >= new Date(that.periods[i].startTime).getTime() && new Date(that.taskData.period[0]).getTime() <= new Date(that.periods[i].endTime).getTime())
+            if(that.taskData.period[0] >= that.periods[i].startTime && that.taskData.period[0] <= that.periods[i].endTime)
+            {
+              
+              that.endTimeRange=[that.taskData.period[0]+' - '+that.periods[i].endTime]
+              break
+            }
+          }
+        }
+      }
+    },
+    changeEndTime() {
+      if(this.first_choose=='' || this.first_choose=='period') {
+        if(this.taskData.date!=null&&this.taskData.period[0]!=null&&this.taskData.period[1]!=null) {
+        //选择了日期
+          this.first_choose='period'
+          var that=this
+          api.findClassrooms({
+            date: that.taskData.date,
+            startTime: that.taskData.period[0],
+            endTime: that.taskData.period[1]
+          }).then( res => {
+            that.classrooms=[]
+            for(var i=0;i<res._embedded.classrooms.length;i++)
+            {
+              that.classrooms.push({label:res._embedded.classrooms[i].name,value:res._embedded.classrooms[i].id})
+            }
+          })
+        }
+      }
+    },
+    changeClassroom() {
+      if(this.first_choose=='' || this.first_choose=='classroom') {
+        //清空或未选择
+        if(this.taskData.classroom=='') {
+          this.first_choose=''
+          this.taskData.date=null
+          this.taskData.period=[null, null]
+          this.startTimeRange=[]
+          this.endTimeRange=[]
+        }
+        else{
+        //选择了教室或者换了教室
+          this.first_choose='classroom'
+          this.taskData.date=null
+          this.taskData.period=[null, null]
+        }
+      }
     }
   }
 }
