d5d3e934b2f90d9a03f33f4cf150435fb07cf1e4
cga <cga20005@163.com>
2022-04-28 11:44:06 +0800

    前端代码更新到coding

--- code/frontend/web/src/views/device_management/status_monitor/status_monitor.vue
+++ code/frontend/web/src/views/device_management/status_monitor/status_monitor.vue
@@ -1,141 +1,118 @@
 <template>
   <div class="app-container">
-    <el-card class="card-box" style="width: 100%">
+    <el-card>
       <div slot="header">
-        <span style="margin-right: 20px">组:</span>
-        <el-select v-model="selectedGroup" placeholder="组">
+        <span style="margin-right: 10px">分组</span>
+        <el-select v-model="selectedGroup" placeholder="选择一个分组">
           <el-option
-            v-for="item in groups"
-            :key="item.id"
-            :label="item.value"
-            :value="item.value"
+            v-for="item in ipGroups"
+            :key="item"
+            :label="item"
+            :value="item"
           >
           </el-option>
         </el-select>
-
-        <el-button style="margin: 5px" @click="handleRefresh">刷新</el-button>
+        <el-button type="primary" style="margin: 5px" @click="getIPAutoConnect"
+          >连接</el-button
+        >
+        <el-button type="warning" style="margin: 5px" @click="handleRefresh"
+          >刷新</el-button
+        >
         <el-button style="margin: 5px" @click="disconnectAll">终止</el-button>
       </div>
       <div>
         <el-collapse v-model="activeCollapseMonitor" @change="handleCollapse">
           <el-collapse-item title="监控" name="0">
-            <div style="width: 100%; height: 100%; float: left;">
-              <div
-                class="image-box"
-                id="screen1"
-                v-show="ScreenIsShow[0]"
-                @click="setFocus(1)"
-                :style="{
-                  width: videoWidth1 + 'px',
-                  height: videoHeight1 + 'px',
-                  float: 'left'
-                }"
-              ></div>
-
-              <div
-                class="image-box"
-                id="screen2"
-                v-show="ScreenIsShow[1]"
-                @click="setFocus(2)"
-                :style="{
-                  width: videoWidth2 + 'px',
-                  height: videoHeight2 + 'px',
-                  float: 'left'
-                }"
-              ></div>
-
-              <div
-                class="image-box"
-                id="screen3"
-                v-show="ScreenIsShow[2]"
-                @click="setFocus(3)"
-                :style="{
-                  width: videoWidth3 + 'px',
-                  height: videoHeight3 + 'px',
-                  float: 'left'
-                }"
-              ></div>
-
-              <div
-                class="image-box"
-                id="screen4"
-                v-show="ScreenIsShow[3]"
-                @click="setFocus(4)"
-                :style="{
-                  width: videoWidth4 + 'px',
-                  height: videoHeight4 + 'px',
-                  float: 'left'
-                }"
-              ></div>
+            <div>
+              <div class="image-div" v-for="n in DivNum" :key="`none-${n}`">
+                <span v-show="ScreenSizeChange[n - 1] == 1">{{
+                  ipInfo[n - 1] != null ? "IP为" + `${ipInfo[n - 1].ip}` : "无"
+                }}</span>
+                <div
+                  :class="{ 'image-box-enlarge': ScreenSizeChange[n - 1] == 1 }"
+                  class="image-box"
+                  :id="`screen_${n - 1}`"
+                  v-if="ReloadDiv"
+                  v-show="ScreenIsShow[n - 1]"
+                  @click="setFocus(n - 1)"
+                />
+              </div>
             </div>
           </el-collapse-item>
         </el-collapse>
       </div>
     </el-card>
 
-    <el-card class="card-box" style="width: 100%">
-      <div>
-        <el-collapse v-model="activeCollapse" @change="handleCollapse">
-          <el-collapse-item title="配置" name="1">
-            <el-row style="margin: 10px">
-              <el-col :span="12">
-                ip:
+    <el-card style="marginTop:5px">
+      <el-collapse v-model="activeCollapse" @change="handleCollapse">
+        <el-collapse-item title="配置" name="1">
+          <div style="margin-left:10px;margin-top:10px">
+            <div>
+              <span class="span-in-card"
+                >IP地址
                 <el-input
-                  id="ip"
+                  class="input-in-card"
                   v-model="ipInput.ip"
-                  style="width:80%;margin:5px"
-                />
-              </el-col>
-              <el-col :span="8">
-                port:
+                  placeholder="账号"
+                ></el-input>
+              </span>
+              <span class="span-in-card"
+                >端口
                 <el-input
-                  id="port"
+                  class="input-in-card"
                   v-model="ipInput.port"
-                  style="width:80%;margin:5px"
-                />
-              </el-col>
-              <el-col :span="8">
-                组:
+                  placeholder="密码"
+                ></el-input
+              ></span>
+              <span class="span-in-card"
+                >分组
                 <el-input
-                  id="port"
+                  class="input-in-card"
                   v-model="ipInput.monitorGroup"
-                  style="width:80%;margin:5px"
-                />
-              </el-col>
-              <el-col :span="4">
-                <el-button type="primary" style="margin:5px" @click="handleAdd"
-                  >提交</el-button
-                >
-              </el-col>
-            </el-row>
-            <el-row style="margin: 10px">
-              <el-col :span="12">
-                id:
+                  placeholder="分组"
+                ></el-input>
+              </span>
+              <el-button type="primary" @click="handleAdd">添加</el-button>
+            </div>
+            <div>
+              <span class="span-in-card">
+                删除ID
                 <el-input
                   id="ip"
                   v-model="ipSelDel"
                   style="width:80%;margin:5px"
+                  placeholder="输入ID删除对应项"
                 />
-              </el-col>
-              <el-col :span="6">
+              </span>
+              <el-button type="warning" style="margin:5px" @click="handleDelete"
+                >删除</el-button
+              >
+            </div>
+          </div>
+
+          <el-table :data="ipInfo">
+            <el-table-column prop="id" label="ID" width="60" sortable />
+            <el-table-column prop="ip" label="被监控机ip" min-width="200" />
+            <el-table-column prop="port" label="端口" width="100" />
+            <el-table-column
+              prop="monitorgroup"
+              label="分组"
+              min-width="100"
+              sortable
+            />
+            <el-table-column fixed="right" label="操作" width="60">
+              <template slot-scope="scope">
                 <el-button
-                  type="primary"
-                  style="margin:5px"
-                  @click="handleDelete"
+                  type="text"
+                  size="small"
+                  @click="delMonitorButton(scope.row.id)"
                   >删除</el-button
                 >
-              </el-col>
-            </el-row>
-
-            <el-table class="custom-table" :data="ipInfo" border>
-              <el-table-column prop="id" label="ID" width="60" />
-              <el-table-column prop="ip" label="被监控机ip" width="180" />
-              <el-table-column prop="port" label="端口" width="180" />
-              <el-table-column prop="monitorgroup" label="组" width="180" />
-            </el-table>
-          </el-collapse-item>
-        </el-collapse>
-      </div>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-collapse-item>
+      </el-collapse>
     </el-card>
   </div>
 </template>
@@ -147,38 +124,29 @@ import { getAllIP, addIP, delIP } from "@/api/IPservice/IPservice";
 export default {
   data() {
     return {
-      groups: [],
-      groupId: 0,
+      DivNum: 9,
+      ipGroups: [],
       selectedGroup: "",
-      ipSelDel: 1,
+      ipSelDel: null,
       ipInput: { ip: "192.168.1.", port: 9000, monitorGroup: "group1" },
       ipInfo: [],
-      SignalScreenSizeChange: [0, 0, 0, 0],
-      ScreenIsShow: [true, true, true, true],
+      ScreenSizeChange: [],
+      ScreenIsShow: [],
       wsURL: "127.0.0.1",
       RFBPool: [],
       PortWebSocket: 9000,
-      wscreen: [
-        {
-          scre: "screen1"
-        },
-        {
-          scre: "screen2"
-        },
-        {
-          scre: "screen3"
-        },
-        {
-          scre: "screen4"
-        }
-      ],
 
+      ReloadDiv: true,
       activeCollapse: ["1"],
       activeCollapseMonitor: ["0"]
     };
   },
 
   mounted() {
+    for (let i = 0; i < this.DivNum; i++) {
+      this.ScreenIsShow.push(true);
+      this.ScreenSizeChange.push(0);
+    }
     this.getIP();
   },
 
@@ -186,9 +154,18 @@ export default {
     handleDelete() {
       this.delIP();
     },
-
     handleAdd() {
       this.addIP();
+    },
+
+    delMonitorButton(id) {
+      let data = { id: id };
+      delIP(data).then(res => {
+        this.$message({
+          message: res
+        });
+        this.getIP();
+      });
     },
 
     delIP() {
@@ -216,7 +193,7 @@ export default {
     getIP() {
       getAllIP().then(response => {
         this.ipInfo = response;
-        this.processGroup();
+        this.getGroup();
       });
     },
 
@@ -227,45 +204,14 @@ export default {
       });
     },
 
-    processGroup() {
-      var s = this.ipInfo.length;
-      var isRepeat = false;
-      for (let i = 0; i < s; i = i + 1) {
-        for (let gd = 0; gd < this.groups.length; gd = gd + 1) {
-          if (this.groups[gd].value === this.ipInfo[i].monitorgroup) {
-            isRepeat = true;
-            break;
-          }
+    getGroup() {
+      for (var i = 0; i < this.ipInfo.length; i++) {
+        if (this.ipGroups.indexOf(this.ipInfo[i].monitorgroup) == -1) {
+          this.ipGroups.push(this.ipInfo[i].monitorgroup);
         }
-        if (isRepeat === false) {
-          let pushData = {
-            id: this.groupId,
-            value: this.ipInfo[i].monitorgroup
-          };
-          this.groupId = this.groupId + 1;
-          this.groups.push(pushData);
-        }
-        isRepeat = false;
       }
     },
 
-    // vnc连接断开的回调函数
-    //  disconnectedFromServer(msg) {
-    //    if (msg.detail.clean) {
-    //      // 根据 断开信息的msg.detail.clean 来判断是否可以重新连接
-    //      //this.connectVnc()
-    //    } else {
-    //      //这里做不可重新连接的一些操作
-    //      //console.log('链接失败,重新链接中-------' + this.wsURL)
-    //      //this.connectVnc()
-    //    }
-    //  },
-    //  // 连接成功的回调函数
-    //  connectedToServer(msg) {
-    //    // console.log('success', msg)
-    //  },
-
-    //连接vnc的函数
     connectVnc() {
       const PASSWORD = "4396";
       let needConnect = [];
@@ -275,24 +221,15 @@ export default {
         }
       }
       let SessionNum = needConnect.length;
-      //  console.log(`read ${SessionNum} ip to connect.`);
       for (var i = 0; i < SessionNum && i < 4; i++) {
-        //  if(this.RFBPool[i]!==null){
-        //    this.RFBPool[i]._disconnect();
-        //    this.RFBPool[i]=null;
-        //    document.getElementById(this.wscreen[i].scrt).innerHTML='';
-        //  }
-        //  this.RFBPool[i]=null;
         let url =
           "ws://" +
           needConnect[i].ip +
           ":" +
           this.PortWebSocket.toString() +
           "/websockify";
-        //  this.PortWebSocket = this.PortWebSocket + 1;
-        console.log("连接至" + url);
         var newRFB = new RFB(
-          document.getElementById(this.wscreen[i].scre),
+          document.getElementById("screen_" + (i + 1)),
           url,
           {
             credentials: { password: PASSWORD }
@@ -302,7 +239,6 @@ export default {
         newRFB._scaleViewport = true;
         newRFB._resizeSession = true;
         this.RFBPool.push(newRFB);
-        //   document.getElementById(this.wscreen[i].scrt).innerHTML = this.chosM[i];
       }
     },
 
@@ -313,6 +249,7 @@ export default {
           this.RFBPool[i] = null;
         }
       }
+      this.reload();
     },
 
     handleRefresh() {
@@ -320,96 +257,51 @@ export default {
       this.connectVnc();
     },
 
-    //handleConnectChange() {
-    //  this.disconnectAll();
-    //  var i = 0;
-    //  var size = this.ipInfo.length; //BUG
-    //  for (i = 0; i < size; i = i + 1) {
-    //    if (this.ipInfo[i] != null) {
-    //      var tempIpList = this.ipInfo[i].ip.split(".");
-    //      if (tempIpList[2] === this.ScreenGroup) {
-    //        this.ipReadyToConnect.push(this.ipInfo[i]);
-    //      }
-    //    }
-    //  }
-    //  //  this.connectSixVnc();
-    //},
-
-    signalScreenSizeChangeUpdateOne(current) {
-      var i = 0;
-      for (i = 0; i < this.SignalScreenSizeChange.length; i = i + 1) {
-        if (i !== current) {
-          this.$set(this.SignalScreenSizeChange, i, 0);
-        } else {
-          //  console.log("set SSSC " + i + " to true");
-          this.$set(this.SignalScreenSizeChange, i, 1);
-        }
-      }
-    },
-
-    screenIsShowUpdateOne(current) {
-      var i = 0;
-      for (i = 0; i < this.ScreenIsShow.length; i = i + 1) {
+    screenShowOne(current) {
+      for (let i = 0; i < this.ScreenIsShow.length; i = i + 1) {
         if (i !== current) {
           this.$set(this.ScreenIsShow, i, false);
         } else {
-          //  console.log("set SIS " + i + " to true");
           this.$set(this.ScreenIsShow, i, true);
         }
       }
     },
 
-    screenUpdateShowAll(current) {
-      var i = 0;
-      for (i = 0; i < this.ScreenIsShow.length; i = i + 1) {
-        if (i !== current) {
-          this.$set(this.ScreenIsShow, i, true);
-        } else {
-          this.$set(this.SignalScreenSizeChange, i, 0);
+    screenShowAll(current) {
+      for (let i = 0; i < this.ScreenIsShow.length; i = i + 1) {
+        this.$set(this.ScreenIsShow, i, true);
+      }
+    },
+
+    setFocus(scrid) {
+      for (let i = 0; i < this.DivNum; i = i + 1) {
+        if (i === scrid) {
+          //显示控制
+          if (this.ScreenIsShow.indexOf(false) == -1) {
+            //没有false表明没有focus
+            this.screenShowOne(i);
+          } else {
+            this.screenShowAll(i);
+          }
+
+          //大小控制
+          if (this.ScreenSizeChange[i] === 0) {
+            this.$set(this.ScreenSizeChange, i, 1);
+          } else {
+            this.$set(this.ScreenSizeChange, i, 0);
+          }
         }
       }
     },
 
-    setFocus: function(scrid) {
-      switch (scrid) {
-        case 1:
-          if (this.SignalScreenSizeChange[0] == 0) {
-            this.signalScreenSizeChangeUpdateOne(0);
-            this.screenIsShowUpdateOne(0);
-          } else {
-            this.screenUpdateShowAll(0);
-          }
-          break;
-        case 2:
-          if (this.SignalScreenSizeChange[1] == 0) {
-            this.signalScreenSizeChangeUpdateOne(1);
-            this.screenIsShowUpdateOne(1);
-          } else {
-            this.screenUpdateShowAll(1);
-          }
-          break;
-        case 3:
-          if (this.SignalScreenSizeChange[2] == 0) {
-            this.signalScreenSizeChangeUpdateOne(2);
-            this.screenIsShowUpdateOne(2);
-          } else {
-            this.screenUpdateShowAll(2);
-          }
-          break;
-        case 4:
-          if (this.SignalScreenSizeChange[3] == 0) {
-            this.signalScreenSizeChangeUpdateOne(3);
-            this.screenIsShowUpdateOne(3);
-          } else {
-            this.screenUpdateShowAll(3);
-          }
-          break;
-      }
+    reload() {
+      this.ReloadDiv = false;
+      this.$nextTick(() => {
+        this.ReloadDiv = true;
+      });
     },
 
-    handleCollapse(val) {
-      //  console.log(val);
-    }
+    handleCollapse(val) {}
   },
 
   computed: {
@@ -448,63 +340,35 @@ export default {
           return 600;
         }
       }
-    },
-    videoWidth3: {
-      get: function() {
-        if (this.SignalScreenSizeChange[2] == 0) {
-          return 300;
-        } else if (this.SignalScreenSizeChange[2] == 1) {
-          return 800;
-        }
-      }
-    },
-    videoHeight3: {
-      get: function() {
-        if (this.SignalScreenSizeChange[2] == 0) {
-          return 300;
-        } else if (this.SignalScreenSizeChange[2] == 1) {
-          return 600;
-        }
-      }
-    },
-    videoWidth4: {
-      get: function() {
-        if (this.SignalScreenSizeChange[3] == 0) {
-          return 300;
-        } else if (this.SignalScreenSizeChange[3] == 1) {
-          return 800;
-        }
-      }
-    },
-    videoHeight4: {
-      get: function() {
-        if (this.SignalScreenSizeChange[3] == 0) {
-          return 300;
-        } else if (this.SignalScreenSizeChange[3] == 1) {
-          return 600;
-        }
-      }
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.card-box {
-  width: 400px;
-  max-width: 100%;
-  margin: 20px auto;
+.input-in-card {
+  width: 30vh;
+  margin: 5px;
 }
-
+.span-in-card {
+  margin-left: 10px;
+  display: inline-block;
+}
+.image-div {
+  margin: 2px;
+  margin-right: 10px;
+  display: inline-block;
+}
 .image-box {
-  width: 100%;
-  height: 100%;
-  // border: 5px solid red;
-  background: rgb(54, 54, 54);
-  margin: 10px;
+  height: 30vh;
+  width: 40vh;
+  border-radius: 5px;
+  background: rgba(73, 73, 73, 0.137);
 }
-
-.custom-table {
-  margin: 10px;
+.image-box-enlarge {
+  height: 60vh;
+  width: 80vh;
+  border-radius: 5px;
+  background: rgba(33, 45, 83, 0.281);
 }
 </style>
